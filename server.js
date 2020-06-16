0b5638e348817c857771c123aa4115ebd1d79c0e
hintzetyson <46944160+hintzetyson@users.noreply.github.com>
2020-06-15 19:48:28 -0600

    Fixed some minor issues

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "Iamtheman420!!!",
+    password: "",
     database: "company_db"
 });
 
